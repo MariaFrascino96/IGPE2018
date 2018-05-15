d84e82ed5453bb140494ab1ad50f45a9ff750c0e
frufo <frufo@DESKTOP-Q7II345>
2018-05-15 14:36:59 +0200

    fsd

--- Brothers/src/main/java/coreMain/Main.java
+++ Brothers/src/main/java/coreMain/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		Matrice matrice = new Matrice();
 		JFrame frame = new MyFrame();
-		frame.setVisible(true);
+		//frame.setVisible(true);
 		Mondo mondo = new Mondo(matrice);
 	}
 
